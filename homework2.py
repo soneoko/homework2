8cf2e18b0527e0aece8c45d879b3fcd6675f5c43
soneoko <Sofya.kayumova01@gmail.com>
2024-07-09 02:30:54 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+homeworkCount = 12
+hoursCount = 1.5
+nameOfCourse = 'Python'
+print(nameOfCourse, ', всего задач:', homeworkCount, ', затрачено часов:', hoursCount, ', среднее время выполнения ', hoursCount/homeworkCount, ' часа.', sep='')
